0340ae5d535983d18205bb597c364ce357f2ad01
Maria-Eduarda-Strunz-Seawright <106779542+Maria-Eduarda-Strunz-Seawright@users.noreply.github.com>
2023-09-13 16:20:07 -0400

    MenuPrincipalUI
    Botões, Textinhos e etc (UI)

--- universal-fantasy-academia/Assets/Scripts/Players/PlayerInteract/InteractSensor.cs
+++ universal-fantasy-academia/Assets/Scripts/Players/PlayerInteract/InteractSensor.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class InteractSensor : MonoBehaviour
 {
-    ArrayList items = new ArrayList();
 
     void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Interactable"))
         {
-            items.Add();
+           
         }
     }
 
