84fc0e9ac1211a8cdf2a3817971bfb787ab0c349
Victor E B Barros <99879937+victoremanuel01@users.noreply.github.com>
2023-10-24 19:25:32 -0300

    commit

--- universal-fantasy-academia/Assets/Scriptable Objects/Scripts/Collections/ItemDropController.cs
+++ universal-fantasy-academia/Assets/Scriptable Objects/Scripts/Collections/ItemDropController.cs
@@ -9,6 +9,9 @@ public class ItemDropController : Interactable
     [Range(1, 10)]
     public int quantity = 1;
 
+    public Animator animator;
+    public string actionBoolAnimator;
+
     private GameObject droppedItem;
 
     private bool isDropped = false;
@@ -21,6 +24,11 @@ public class ItemDropController : Interactable
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
     }
 
+    public void PlayActionAnimator()
+    {
+        animator.SetTrigger(actionBoolAnimator);
+    }
+
 
 
     void Start()
@@ -30,19 +38,29 @@ public class ItemDropController : Interactable
 
     public void OnDrop()
     {
+
         if (isDropped)
         {
             return;
         }
-        
+
+        if(animator != null)
+        {
+            PlayActionAnimator();
+        }
+
         for (int i = 0; i < quantity; i++)
         {
-            Vector3 playerPosition = player.gameObject.transform.position;
+//            Vector3 playerPosition = player.gameObject.transform.position;
+//
+//            float x = Random.Range(playerPosition.x - 2, playerPosition.x + 2);
+//            float z = Random.Range(playerPosition.z - 2, playerPosition.z + 2);
+//
+//            Vector3 dropPosition = new Vector3(x, playerPosition.y + 1, z);
 
-            float x = Random.Range(playerPosition.x - 2, playerPosition.x + 2);
-            float z = Random.Range(playerPosition.z - 2, playerPosition.z + 2);
+            Vector3 dropPosition = gameObject.transform.GetChild(1).position;
 
-            Vector3 dropPosition = new Vector3(x, playerPosition.y + 1, z);
+            prefab.GetComponent<Animator>().enabled = true;
 
             droppedItem = Instantiate(prefab, dropPosition, Quaternion.identity);
             droppedItem.GetComponent<ItemObject>().Item = Item;
